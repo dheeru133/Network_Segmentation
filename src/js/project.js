643456ee37fd5118595cafd1c87e930f2533fd33
Dheeraj Chaudhary <Dheeraj.Chaudhary@contractor.hallmark.com>
2018-04-22 22:28:07 -0500

    Style-changes

--- src/js/project.js
+++ src/js/project.js
@@ -2,120 +2,37 @@
  * @Author: Dheeraj Chaudhary 
  * @Date: 2018-04-06 16:02:09 
  * @Last Modified by: Dheeraj.Chaudhary@contractor.hallmark.com
- * @Last Modified time: 2018-04-10 09:37:50
+ * @Last Modified time: 2018-04-12 16:47:46
  */
 
 // const jQuery = require('jquery');
 jQuery(document).ready(function($) {
-    // Cardousel Trigger
-    $('.carousel').carousel({
-        interval: 1000,
-        keyboard: true,
-        pause: 'hover',
-        wrap: true
-    });
-    //Carousel Trigger
-    var container = $(
-        '<div class="table-menu table-menu-hidden"><ul class="list-unstyled"/></div>'
-    );
+    console.log('Running');
 
-    var table = $('table');
-    // table.addClass('enhanced');
-    // table.addClass('enhanced');
+    // document.getElementById('search').value = 'Sachin';
+    // document.getElementById("search-form").submit();
 
-    debugger;
-    $('thead th').each(function(i) {
-        var th = $(this),
-            id = th.attr('id'),
-            classes = th.attr('class'); // essential, optional (or other content identifiers)
+    // console.log('hi');
+    // setTimeout(function() {
 
-        // assign an ID to each header, if none is in the markup
-        if (!id) {
-            id = 'col-' + i;
-            th.attr('id', id);
-        }
+    //     window.open("https://www.presearch.org/");
 
-        // loop through each row to assign a "headers" attribute and any classes (essential, optional) to the matching cell
-        // the "headers" attribute value = the header's ID
-        $('tbody tr').each(function() {
-            var cell = $(this)
-                .find('th, td')
-                .eq(i);
-            cell.attr('headers', id);
-            if (classes) {
-                cell.addClass(classes);
-            }
-        });
-        // create the menu hide/show toggles
-        if (!th.is('.persist')) {
-            // note that each input's value matches the header's ID;
-            // later we'll use this value to control the visibility of that header and it's associated cells
-            var toggle = $(
-                '<li class="px-2"> <input type="checkbox" name="toggle-cols" id="toggle-col-' +
-                i +
-                '" value="' +
-                id +
-                '" /> <label for="toggle-col-' +
-                i +
-                '">' +
-                th.text() +
-                '</label></li>'
-            );
+    // },  3000);
 
-            // append each toggle to the container
-            container.find('ul').append(toggle);
-            // assign behavior
-            toggle
-                .find('input')
+    // var popup = window.open('https://www.presearch.org', '_top');
 
-            // when the checkbox is toggled
-            .change(function() {
-                    var input = $(this),
-                        val = input.val(), // this equals the header's ID, i.e. "company"
-                        cols = $('#' + val + ', [headers=' + val + ']'); // so we can easily find the matching header (id="company") and cells (headers="company")
+    // setTimeout(function() {
+    //     document.getElementById('search').value = 'Sachin';
+    // }, 3000);
 
-                    if (input.is(':checked')) {
-                        cols.show();
-                    } else {
-                        cols.hide();
-                    }
-                })
-                // custom event that sets the checked state for each toggle based on column visibility, which is controlled by @media rules in the CSS
-                // called whenever the window is resized or reoriented (mobile)
-                .bind('updateCheck', function() {
-                    if (th.css('display') == 'table-cell') {
-                        $(this).attr('checked', true);
-                    } else {
-                        $(this).attr('checked', false);
-                    }
-                })
-                // call the custom event on load
-                .trigger('updateCheck');
-        } // end conditional statement ( !th.is(".persist") )
-    }); // end headers loop
+    // popup.onload
 
-    $(window).bind('orientationchange resize', function() {
-        container.find('input').trigger('updateCheck');
-    });
+    // popup.onload = function() {
+    // init();
+    console.log('After window load');
 
-    var menuWrapper = $('<div class="table-menu-wrapper" />');
-    // menuBtn = $('<a href="#" class="table-menu-btn">Display</a>');
-
-    // var menuWrapper = $('.table-menu-wrapper');
-    // menuBtn = $('.table-menu-btn');
-
-    // menuBtn.click(function() {
-    //     container.toggleClass('table-menu-hidden');
-    //     return false;
-    // });
-
-    menuWrapper.append(container);
-    $('.table-menu-wrapper-custom').append(menuWrapper);
-    // table.before(menuWrapper); // append the menu immediately before the table
-    // assign click-away-to-close event
-    $(document).click(function(e) {
-        if (!$(e.target).is(container) && !$(e.target).is(container.find('*'))) {
-            container.addClass('table-menu-hidden');
-        }
-    });
+    setTimeout(function() {
+        document.getElementById('search').value = 'Sachin';
+    }, 1000);
+    // };
 });
